0636e0d8298e9984a3f4b8803e5fe5218dd2aca2
rotivo <vitorfernando@usp.br>
2022-09-17 11:37:27 -0300

    Testando dicionários no python

---
+++ Dicionario.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+
+dicionario = {"Tinky_Winky":"22", "Dipsy":"43", "Lala":"32", "Poo":"23"}
+print(dicionario)
+
+print(dicionario["Tinky_Winky"], dicionario["Dipsy"], dicionario["Lala"], dicionario["Poo"])
