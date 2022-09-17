01d44e4873abbaffb5c13ca8a91e78152bbc3639
rotivo <vitorfernando@usp.br>
2022-09-17 11:17:02 -0300

    Testando o comando sys.argv

---
+++ Inteiro.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python3
+import sys
+
+print(sys.argv[0])
+a = ("You lured me in and I couldn't sense the pain")
+
+print(sys.argv[1])
+b = ("Shaking, falling onto my knees")
+
+print(sys.argv[2])
+c = ("I'll be needing stitches")
+
+print(a, b, c)
