c779a44bd8457f5ff2bf30c44846a2d03e41242f
rotivo <vitorfernando@usp.br>
2022-09-18 00:53:17 -0300

    Parte 1 do exercício de Python da primeira semana

---
+++ TarefaPython1.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python3
+
+#Imprimindo um texto
+print("Hello, New York")
+
+#Armazenando meu nome em uma variável
+nome = 'Vitor'
+
+#Imprimindo meu nome
+print("Meu nome é", nome)
+
+#Salvando cor favorita em uma variável
+corFavorita = 'todas'
+
+#Imprimindo minha cor favorita
+print("Minha cor favorita é", corFavorita)
+
+#Salvando atividade favorita
+atividadeFavorita = 'dormir'
+
+#Imprimindo atividade favorita
+print("Minha atividade favorita é", atividadeFavorita)
+
+#Salvando animal favorito
+animalFavorito = 'todos'
+
+#Imprindo animal favorito
+print("Meu animal favorito é", animalFavorito)
