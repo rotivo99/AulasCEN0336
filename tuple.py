12f038e8a5276e1d11db2f36db73531aef86873f
rotivo <vitorfernando@usp.br>
2022-09-22 10:27:46 -0300

    Aprendendo sobre tuples no python3

---
+++ tuple.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+
+tuple_b = ("Squirtle", "Charmander", "Bulbassauro", "Pikachu", "Chicorita", "Totodile", "Piplup")
+
+#Testamdp  o tuple no python3
+print(tuple_b[0])
+print(tuple_b[1])
+print(tuple_b[2])
+print(tuple_b[3])
+print(tuple_b[4])
+print(tuple_b[5])
+print(tuple_b[6])
+
+#Designando um novo nome para o 0 do tuple e vendo que dá erro, porque ele não aceita isso, só a lista
+tuple_b[0] = "Blastoise"
+print(tuple_b[0])
+print(tuple_b)
