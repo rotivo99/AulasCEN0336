a89159cf6b4784c5a18b67a97174674dc8fbe757
rotivo <vitorfernando@usp.br>
2022-09-22 10:11:29 -0300

    Testando alguns comandos

---
+++ ArquivoInt.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+#Libraries
+import sys
+
+a = 1
+b = 2
+
+print("Sum numbers:", a+b)
+
+c = int(sys.argv[1])
+print(c)
+print("Printing c variable:", 1234)
+print("Printing c variable:", sys.argv[1])
