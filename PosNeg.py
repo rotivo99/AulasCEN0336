b772e77951d51314139b9531d6fee0bf89040c0f
rotivo <vitorfernando@usp.br>
2022-09-22 13:59:00 -0300

    PosNeg.py

--- PosNeg.py
+++ PosNeg.py
@@ -12,5 +12,9 @@ elif numero > 0:
         print(sys.argv[1], "is a positive number smaller than 50.")
         if numero % 2 == 0:
             print(sys.argv[1], "is an even positive number smaller than 50.")
+    if numero > 50:
+        print(sys.argv[1], "is a positive number bigger than 50.")
+        if numero % 3 == 0:
+            print(sys.argv[1], "is an odd positive number bigger than 50.")
 else:
     print("That's just zero.")
